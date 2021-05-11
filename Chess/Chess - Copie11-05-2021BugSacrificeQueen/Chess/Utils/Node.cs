535aaab099b41ea5010c53f1add2f75949c7179f
val15 <randrenarizoval@gmail.com>
2021-05-11 10:27:41 +0300

    correction bug IA racifice modification du score de Knight en 30

---
+++ Chess/Chess - Copie11-05-2021BugSacrificeQueen/Chess/Utils/Node.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Utils
+{
+  public class Node
+  {
+
+    private List<Pawn> _currentLocalPawnList;
+    public int Level { get; set; }
+    public int Weight { get; set; }
+    public string Location { get; set; }
+    public string OldPositionName { get; set; }
+
+    public string Colore { get; set; }
+
+    public Node Parent { get; set; }
+    public List<Node> ChildList { get; set; }
+
+    public string BestChildPosition { get; set; }
+    public  Pawn AssociatePawn { get; set; }
+
+    //public TreeNode<CircleNode> ParentTreeNode { get; set; }
+
+    public List<Pawn> GetCurrentLocalPawnList()
+    {
+      return _currentLocalPawnList;
+    }
+
+    public List<Pawn> GetCurrentLocalPawnListAllier()
+    {
+      return _currentLocalPawnList.Where(x=>x.Colore == Colore).ToList();
+    }
+    public Node()
+    {
+    }
+
+      public Node(List<Pawn> currentLocalPawnList)
+    {
+      ChildList = new List<Node>();
+      BestChildPosition = "";
+      _currentLocalPawnList = null;
+      _currentLocalPawnList = new List<Pawn>();
+      _currentLocalPawnList.AddRange(currentLocalPawnList);
+    }
+
+  }
+}
